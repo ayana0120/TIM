7293d2977d5de93240af1b8334d89674d3b61ae2
ayana0120 <keichan0501.ayana@gmail.com>
2020-10-06 18:13:05 +0900

    [Add]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  get 'genres/index'
-  get 'items/index'
-  get 'items/new'
-  get 'items/show'
-  get 'items/edit'
-  get 'items/search'
-  get 'users/show'
-  get 'users/top'
-  get 'users/about'
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    sessions: 'devise/sessions',
+    registrations: 'devise/registrations'
+  }
+
+  root "users/top"
+
+  resources :users, only:[:show]
+  get "/about" => "users#about"
+
+  resources :items
+  get "/items/search" => "items#search"
+
+  resources :genre, only:[:index, :create, :update, :destroy]
 end
