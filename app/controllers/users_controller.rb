86779d65c633c65700a08154bf6e55f081940b37
ayana0120 <keichan0501.ayana@gmail.com>
2020-10-06 18:12:12 +0900

    [Add]controller/model/views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def top
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'genres/index'
+  get 'items/index'
+  get 'items/new'
+  get 'items/show'
+  get 'items/edit'
+  get 'items/search'
+  get 'users/show'
+  get 'users/top'
+  get 'users/about'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
