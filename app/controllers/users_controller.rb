4061d0a38c458f064f384f4064b8620cdfebc01e
ayana0120 <keichan0501.ayana@gmail.com>
2020-10-07 23:13:28 +0900

    [Add]bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
-  	@items = Item.all
-    @limit_items = Item.limit(5).order("exp")
+  	@items = Item.all.includes(:genre)
+    @limit_items = Item.limit(5).order("exp").includes(:genre)
     @genres = Genre.all
   end
 
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'fad41f3d3da704deccda992ecc2c53db4ac5f9fd3365cd0778e7c9c6dbadf4ae9213855a7fada1097fbfea5f42196cdc9f992087ff0c34b9efaeca5abb68496d'
