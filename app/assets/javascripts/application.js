cfad881e66eaa8557048bdcc6800c36318743c87
ayana0120 <keichan0501.ayana@gmail.com>
2020-10-08 15:09:22 +0900

    [Add]preview

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,12 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+$(function(){
+    $('#item_image').on('change', function (e) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        $(".image").attr('src', e.target.result);
+    }
+    reader.readAsDataURL(e.target.files[0]); //取得したurlにアップロード画像のurlを挿入
+  });
+});
