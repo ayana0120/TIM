6514fd0f99594984d61cf1085a9fb917c7b631fa
ayana0120 <keichan0501.ayana@gmail.com>
2020-10-25 19:38:22 +0900

    [Update]Bootstrap4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,9 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require jquery
-//= require bootstrap-sprockets
+//= require jquery3
+//= require popper
+//= require bootstrap
 //= require_tree .
 $(document).on('turbolinks:load', function() {
   $(function(){
