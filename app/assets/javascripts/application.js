58cba27a78883bc19de1513fcb37fd3248cb021b
ayana0120 <keichan0501.ayana@gmail.com>
2020-11-06 00:16:36 +0900

    [Update]jsをリロードしないと発火しないエラーを解消するため記述リポジトリを変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -44,3 +44,26 @@ $(document).on('turbolinks:load', function() {
     $('#item_quantity').numberSpinner();
   });
 });
+
+// Web Speech API
+$(document).on('turbolinks:load', function() {
+  const speech = new webkitSpeechRecognition();
+  speech.lang = 'ja-JP';
+
+  //使用する変数を用意
+  const button = document.getElementById('button');
+  const item_name = document.getElementById('item_name');
+
+
+  button.addEventListener('click' , function() {
+      // 音声認識をスタート
+      speech.start();
+  });
+
+
+  speech.addEventListener('result' , function(e) {
+     console.log(e);
+
+     item_name.value = e.results[0][0].transcript;
+  });
+});
