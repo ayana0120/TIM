37b225fe1a57a23fada953d3cdba1908be9d277e
ayana0120 <you@example.com>
2021-05-01 00:05:18 +0900

    [Fix]スマホでのジャンル一覧表示方法を変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_25_044742) do
+ActiveRecord::Schema.define(version: 2021_03_17_125949) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
@@ -50,10 +50,6 @@ ActiveRecord::Schema.define(version: 2020_10_25_044742) do
     t.datetime "updated_at", null: false
     t.string "uid"
     t.string "provider"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
